837bc84b57a722344e053d8b6177b6534e0e7940
caviles913 <christopher.avi@edu.312school.com>
2025-12-17 00:23:32 -0600

    Fixing internet gateway

--- vpc_module/nat.tf
+++ vpc_module/nat.tf
@@ -12,7 +12,7 @@ resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public_zone1.id
 
-  depends_on = [aws_internet_gateway.igw] # ensure IGW is ready
+  depends_on = [aws_internet_gateway.gw] # ensure IGW is ready
 
   tags = {
     Name = "${local.env}-nat-gateway" ###staging-nat-gateway
