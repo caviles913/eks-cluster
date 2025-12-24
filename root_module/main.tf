cfc3b754575cd6c328a83b073c17a8687f3aa4c8
caviles913 <christopher.avi@edu.312school.com>
2025-12-24 03:57:57 -0600

    Fixed linting

--- root_module/main.tf
+++ root_module/main.tf
@@ -1,18 +1,18 @@
 
 
 module "main_vpc" {
-  source          = "../vpc_module"
-  vpc_cidr_block  = var.root_vpc_cidr_block
-  environment     = var.root_environment
-  aws_region      = var.root_aws_region
-  aws_zone1       = var.root_aws_zone1
-  aws_zone2       = var.root_aws_zone2
+  source         = "../vpc_module"
+  vpc_cidr_block = var.root_vpc_cidr_block
+  environment    = var.root_environment
+  aws_region     = var.root_aws_region
+  aws_zone1      = var.root_aws_zone1
+  aws_zone2      = var.root_aws_zone2
 
 }
 
 
 module "eks_cluster" {
-  source = "../eks"
+  source              = "../eks"
   aws_cluster_name    = var.root_aws_eks_name
   aws_cluster_version = var.root_eks_version
 }
--- vpc_module/locals.tf
+++ vpc_module/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  cidr_block  = var.vpc_cidr_block
-  env         = var.environment
-  region      = var.aws_region
-  zone1       = var.aws_zone1
-  zone2       = var.aws_zone2
+  cidr_block = var.vpc_cidr_block
+  env        = var.environment
+  region     = var.aws_region
+  zone1      = var.aws_zone1
+  zone2      = var.aws_zone2
 }
