e8f38f44b88c953e5b85e6105523ffcd66945d23
caviles913 <christopher.avi@edu.312school.com>
2025-12-24 04:55:00 -0600

    Fixed errors

--- root_module/main.tf
+++ root_module/main.tf
@@ -1,13 +1,13 @@
 
 
 module "main_vpc" {
-  source         = "../vpc_module"
-  vpc_cidr_block = var.root_vpc_cidr_block
-  environment    = var.root_environment
-  aws_region     = var.root_aws_region
-  aws_zone1      = var.root_aws_zone1
-  aws_zone2      = var.root_aws_zone2
-  aws_eks_name = module.eks_cluster.eks_cluster_name
+  source          = "../vpc_module"
+  vpc_cidr_block  = var.root_vpc_cidr_block
+  environment     = var.root_environment
+  aws_region      = var.root_aws_region
+  aws_zone1       = var.root_aws_zone1
+  aws_zone2       = var.root_aws_zone2
+  aws_eks_name    = module.eks_cluster.eks_cluster_name
   aws_eks_version = module.eks_cluster.eks_cluster_version
 
 }
